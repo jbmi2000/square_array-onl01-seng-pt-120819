e5689ca51b9f58e6c1719d30287a0a4066c465c6
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-18 17:14:50 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+require 'pry'
+numbers = [1,2,3,4]
+
+def square_array(numbers)
+new_array =[]
+numbers.each do |num|
+   new_array << num*num
+#  new_array <<
+end
+new_array
+
 end
